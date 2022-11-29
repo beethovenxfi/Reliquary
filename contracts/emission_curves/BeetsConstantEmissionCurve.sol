6a04c878b7d48847810885f92e083694fabab730
franz <franzns11@gmail.com>
2022-11-29 12:28:08 +0100

    add beets emission curve

---
+++ contracts/emission_curves/BeetsConstantEmissionCurve.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+import "../interfaces/IEmissionCurve.sol";
+import "openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract BeetsConstantEmissionCurve is IEmissionCurve, AccessControl {
+    event EmissionUpdate(uint256 rewardsPerSecond);
+
+    /// @notice Access control roles.
+    bytes32 public constant OPERATOR = keccak256("OPERATOR");
+
+    uint256 public rewardPerSecond;
+
+    constructor(uint256 _rewardPerSecond) {
+        rewardPerSecond = _rewardPerSecond;
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function getRate(uint256 lastRewardTime)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return rewardPerSecond;
+    }
+
+    function setRate(uint256 _rewardPerSecond) external onlyRole(OPERATOR) {
+        rewardPerSecond = _rewardPerSecond;
+        emit EmissionUpdate(_rewardPerSecond);
+    }
+}
